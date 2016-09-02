0e9ccc6d203aa04ab344bdcacd72df39290d2448
Ian Ward <ian@excess.org>
2016-09-02 17:52:43 -0400

    sysadmin access required

--- ckanext/configedit/controller.py
+++ ckanext/configedit/controller.py
@@ -1,5 +1,14 @@
-from ckan.plugins.toolkit import BaseController
+from ckan.plugins.toolkit import (
+    _, BaseController, check_access, NotAuthorized, abort,
+    )
 
 class ConfigEditController(BaseController):
+    def __before__(self, action, **params):
+        super(ConfigEditController, self).__before__(action, **params)
+        try:
+            check_access('sysadmin', {})
+        except NotAuthorized:
+            abort(403, _('Need to be system administrator to administer'))
+
     def edit_config(self):
         return 'pass'
