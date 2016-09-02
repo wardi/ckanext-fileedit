522b0e48341b85ae964f8b201d26d58982c7caaf
Ian Ward <ian@excess.org>
2016-09-02 17:23:05 -0400

    start of controller

---
+++ ckanext/configedit/controllers.py
@@ -0,0 +1,5 @@
+from ckan.plugins.toolkit import BaseController
+
+class ConfigEditController(BaseController):
+    def edit_config(self):
+        return 'pass'
--- ckanext/configedit/plugins.py
+++ ckanext/configedit/plugins.py
@@ -1,7 +1,18 @@
-from ckan.plugins import toolkit, IConfigurer, SingletonPlugin, implements
+import ckan.plugins as p
 
-class ConfigEditPlugin(SingletonPlugin):
-    implements(IConfigurer)
+class ConfigEditPlugin(p.SingletonPlugin):
+    p.implements(p.IConfigurer)
+    p.implements(p.IRoutes, inherit=True)
 
     def update_config(self, config):
-        pass
+        p.toolkit.add_ckan_admin_tab(config, 'edit_config', 'Edit Schema')
+
+    def before_map(self, m):
+        m.connect(
+            'edit_config',
+            '/ckan-admin/edit-config',
+            action='edit_config',
+            ckan_icon='wrench',
+            controller='ckanext.configedit.controllers:ConfigEditController',
+            )
+        return m
