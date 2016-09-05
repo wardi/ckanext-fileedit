a8450deceed5d1f81d83068ad0901b44a2890a3e
Ian Ward <ian@excess.org>
2016-09-05 12:19:53 -0400

    show editable files, make button, label generic

--- ckanext/fileedit/controller.py
+++ ckanext/fileedit/controller.py
@@ -2,6 +2,9 @@ from ckan.plugins.toolkit import (
     _, BaseController, check_access, NotAuthorized, abort, render,
     )
 
+from ckanext.fileedit.config import editable_files
+
+
 class FileEditController(BaseController):
     def __before__(self, action, **params):
         super(FileEditController, self).__before__(action, **params)
@@ -12,5 +15,5 @@ class FileEditController(BaseController):
 
     def edit_file(self):
         return render('fileedit/edit.html', extra_vars={
-            'data': {}, 'errors': {}
+            'data': {}, 'errors': {}, 'editable_files': editable_files,
             })
