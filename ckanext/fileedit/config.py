0f43ce8e5169a410441d30cbebea0d616cac9c10
Ian Ward <ian@excess.org>
2016-09-05 12:02:03 -0400

    load config setting

---
+++ ckanext/fileedit/config.py
@@ -0,0 +1,11 @@
+# plugin configuration, set up/overwritten on each call to
+# FileEditPlugin.configure()
+
+editable_files = [
+#   {
+#       'path': '/location/of/file/to/edit',
+#       'label': 'Display Name',
+#       'validate': validation_function,
+#       'after_save:': reload_function,
+#   }, ...
+]
--- ckanext/fileedit/plugin.py
+++ ckanext/fileedit/plugin.py
@@ -1,19 +1,51 @@
+from os.path import basename
+from importlib import import_module
+
 import ckan.plugins as p
+import yaml
+
+from ckanext.fileedit.config import editable_files
 
 class FileEditPlugin(p.SingletonPlugin):
     p.implements(p.IConfigurer)
+    p.implements(p.IConfigurable)
     p.implements(p.IRoutes, inherit=True)
 
     def update_config(self, config):
         p.toolkit.add_ckan_admin_tab(config, 'edit_file', 'File Editor')
         p.toolkit.add_template_directory(config, 'templates')
 
+    def configure(self, config):
+        editable_files[:] = []  # in-place
+        for c in yaml.load(config['file_edit.editable_files']):
+            editable_files.append({
+                'path': c['path'],
+                'label': c.get('label', basename(c['path'])),
+                'validate': import_symbol(c.get('validate')) or nothing,
+                'after_save': import_symbol(c.get('after_save')) or nothing,
+            })
+
     def before_map(self, m):
         m.connect(
             'edit_file',
-            '/ckan-admin/edit-config',
+            '/ckan-admin/file-edit',
             action='edit_file',
             ckan_icon='wrench',
             controller='ckanext.fileedit.controller:FileEditController',
             )
         return m
+
+
+def import_symbol(symbol_path):
+    '''
+    Given a path like "ckanext.mytheme.validators:file_validator"
+    import the symbol file_validator from ckanext.mytheme.validators.
+
+    Returns None if symbol_path is None.
+    '''
+    if symbol_path:
+        module, symbol = symbol_path.split(':', 1)
+        return getattr(import_module(module), symbol)
+
+def nothing(*args, **kwargs):
+    'do nothing'
